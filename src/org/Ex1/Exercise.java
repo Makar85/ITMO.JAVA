df6e472ef54effdf8ab13ee35087d7c17887e2eb
Makar85 <Levitin85@yandex.ru>
2022-04-05 13:33:19 +0300

    Initial commit

---
+++ src/org/Ex1/Exercise.java
@@ -0,0 +1,41 @@
+package org.Ex1;
+
+public class Exercise {
+    public static void main(String[] args) {
+        System.out.println("-----------1-----------");
+
+        System.out.println("Я");
+        System.out.println("хорошо");
+        System.out.println("знаю");
+        System.out.println("Java.");
+
+        System.out.println("-----------2-----------");
+
+        double var1 = ( 46 + 10 ) * ( 10d / 3 );
+        System.out.println(var1);
+        int var2 = ( 29 ) * ( 4 ) * (- 15 );
+        System.out.println(var2);
+
+        System.out.println("-----------3-----------");
+
+        int number = 10500;
+        double result = (number / 10) / 10;
+        System.out.println(result);
+
+        System.out.println("-----------4-----------");
+
+        double a = 3.6;
+        double b = 4.1;
+        double c = 5.9;
+        double result1 = a*b*c;
+        System.out.println(result1);
+
+        System.out.println("-----------5-----------");
+
+
+
+
+
+
+    }
+}
