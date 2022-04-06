1cd0c533301e980031542c38908292a896d4d27a
Makar85 <Levitin85@yandex.ru>
2022-04-06 16:45:05 +0300

    Добавил задания 4 и 5 в первом блоке

--- src/org/Ex1/Exercise.java
+++ src/org/Ex1/Exercise.java
@@ -1,7 +1,12 @@
 package org.Ex1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Exercise {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("-----------1-----------");
 
         System.out.println("Я");
@@ -31,6 +36,33 @@ public class Exercise {
         System.out.println(result1);
 
         System.out.println("-----------5-----------");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int first = Integer.parseInt(reader.readLine());
+        System.out.println(first);
+        int second = Integer.parseInt(reader.readLine());
+        System.out.println(second);
+        int third = Integer.parseInt(reader.readLine());
+        System.out.println(third);
+
+        System.out.println("-----------6-----------");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int b1 = scanner.nextInt();
+        if (b1%2 !=0){
+            System.out.println(b1 + " Нечетное число");
+        }
+        else if (b1%2 ==0 && b1 < 100)
+        {
+            System.out.println(b1 + " Четное число");
+        }
+        else if (b1%2 ==0 && b1 > 100)
+        {
+            System.out.println("Выход за пределы диапазона");
+        }
+
+
 
 
 
