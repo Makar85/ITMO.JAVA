412710eb77bcab0a6acef47802f03a3f4b0a1239
Makar85 <Levitin85@yandex.ru>
2022-04-07 15:35:07 +0300

    Добавил второй блок

--- src/org/Ex2/Person.java
+++ src/org/Ex2/Person.java
@@ -10,6 +10,30 @@ public class Person {
     public Person() {
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
     public Person(String name, Integer age, Date birthDate) {
         this.name = name;
         this.age = age;
